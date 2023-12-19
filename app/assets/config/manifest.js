e00f6f83ca2ad40bc26fec4c1e500fabc0eb7f72
Evans-Munatsa <evansmunatsa7@gmail.com>
2023-12-19 17:40:26 +0200

    big changes

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../fonts
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link style.css
+//= link datepicker.css
+//= link fancybox.min.css
---
+++ app/javascript/application.js
@@ -0,0 +1,10 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "jquery"
+import "jquery_ujs"
+import "./jquery_ui"
+import "@hotwired/turbo-rails"
+import "controllers"
+import "main"
+import "swiper"
+import "sticky"
+import "datepicker"
